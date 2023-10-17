8ac3ef0b36a7f820e85a4ac6f0464ff49c558620
Tar0k <73.tarok@gmail.com>
2023-10-17 17:31:18 +0400

    init

---
+++ OOP_Seminar7/Calculator.cs
@@ -0,0 +1,58 @@
+﻿namespace OOP_Seminar7;
+
+public class Calculator
+{
+    public static void Start()
+    {
+        var resume = true;
+        while (resume)
+        {
+            resume = Cycle();
+        }
+    }
+    
+    
+    private static bool Cycle()
+    {
+        var input = RequestInput();
+        DoOperation(input.Item1, input.Item2, input.Item3);
+        
+        Console.Write("Выход? Yes : ");
+        var decision = Console.ReadLine();
+        return decision?.ToLower() != "yes";
+    }
+
+    private static (ComplexNumber, ComplexNumber, string?) RequestInput()
+    {
+        
+        Console.Write("Введите первое число: ");
+        var firstNumber = new ComplexNumber(Console.ReadLine());
+        
+        Console.Write("Введите второе число: ");
+        var secondNumber = new ComplexNumber(Console.ReadLine());
+        
+        Console.Write("Введите операцию: ");
+        var op = Console.ReadLine();
+
+        return (firstNumber, secondNumber, op);
+    }
+    
+    private static void DoOperation(ComplexNumber num1, ComplexNumber num2, string? operation)
+    {
+        switch (operation)
+        {
+            case "+":
+                Console.WriteLine(num1 + num2);
+                break;
+            case "*":
+                Console.WriteLine(num1 * num2);
+                break;
+            case "/":
+                Console.WriteLine(num1 / num2);
+                break;
+            default:
+                Console.WriteLine("Данной операции не заложено");
+                break;
+        }
+    }    
+}
---
+++ OOP_Seminar7/ComplexNumber.cs
@@ -0,0 +1,77 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace OOP_Seminar7;
+
+
+public partial class ComplexNumber
+{
+    private readonly double _realPart;
+    private readonly double _imaginePart;
+
+    public ComplexNumber(string complexNumber)
+    {
+        _realPart = GetPart(complexNumber, RealPartRegex());
+        _imaginePart = GetPart(complexNumber, ImaginePartRegex());
+    }
+
+    private ComplexNumber(double realPart, double imaginePart)
+    {
+        _realPart = realPart;
+        _imaginePart = imaginePart;
+    }
+
+    private static double GetPart(string complexNumber, Regex regex)
+    {
+        var res = regex.Match(complexNumber.Replace(" ", ""));
+        if (res.Length == 0)
+        {
+            throw new ArgumentException($"В числе \"{complexNumber}\" не выявлена часть");
+        }
+        return double.TryParse(res.Value, out var result) ? result : throw new InvalidCastException("Не удалось конверитровать часть");
+    }
+
+    public override string ToString()
+    {
+        var realPartText = _realPart > 0 ? _realPart.ToString(CultureInfo.InvariantCulture) : $"- {Math.Abs(_realPart)}";
+        var imaginePartText =
+            _imaginePart > 0 ? $"+ {_imaginePart}i" : $"- {Math.Abs(_imaginePart)}i";
+        return $"{realPartText} {imaginePartText}";
+    }
+    
+    public static ComplexNumber operator + (ComplexNumber num1, ComplexNumber num2)
+    {
+        return new ComplexNumber(
+            realPart: num1._realPart + num2._realPart,
+            imaginePart: num1._imaginePart + num2._imaginePart);
+    }
+     
+    public static ComplexNumber operator * (ComplexNumber num1, ComplexNumber num2)
+    {
+        var realPart = num1._realPart * num2._realPart - num1._imaginePart * num2._imaginePart;
+        var imaginePart = num1._realPart * num2._imaginePart + num1._imaginePart * num2._realPart;
+        return new ComplexNumber(
+            realPart: realPart,
+            imaginePart: imaginePart);
+    }
+    
+    
+    public static ComplexNumber operator / (ComplexNumber num1, ComplexNumber num2)
+    {
+        var realPart = (num1._realPart * num2._realPart + num1._imaginePart * num2._imaginePart) /
+                       (Math.Pow(num2._realPart, 2) + Math.Pow(num2._imaginePart, 2));
+        var imaginePart = (num2._realPart * num1._imaginePart - num1._realPart * num2._imaginePart) /
+            (Math.Pow(num2._realPart, 2) + Math.Pow(num2._imaginePart, 2));
+        return new ComplexNumber(
+            realPart: realPart,
+            imaginePart: imaginePart);
+    }
+
+    [GeneratedRegex(@"[+-]?(\d+?)\b")]
+    private static partial Regex RealPartRegex();
+    
+    [GeneratedRegex(@"[+-]?(\d+?)(?=i)")]
+    private static partial Regex ImaginePartRegex();
+}
+
+
